02fef586154a1770b7189a8b5402e75e150c1695
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2023-03-27 11:26:26 -0300

    Iniciando a parte do main - adicionando elementos

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use stric'
+
+//alert("js works")
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Creater Header
+const createHeader = function(){
+    let contentHeader = document.createElement("div");
+    contentHeader.textContent = "Lion School"
+    contentHeader.className = "heather_content"
+    content.appendChild(contentHeader) 
+}
+createHeader()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//creates footer
+
+const createFooter = function(){
+    let contentFooter = document.createElement("div");
+    contentFooter.textContent = "Copyright © 2022 | Layla Giovanna | A. Beatriz Landi"
+    contentFooter.className = "footer_content"
+    content.appendChild(contentFooter) 
+}
+
+/* createFooter() */

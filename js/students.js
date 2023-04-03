3f572388370259111d34e20f4f4aadac1bcb122a
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2023-04-03 11:27:27 -0300

    criando os cards da segunda tela

--- js/students.js
+++ js/students.js
@@ -1,1 +1,15 @@
+'use strict'
+// import { alunos } from "../js/alunos.js"
+
+/* let card = document.createElement('div')
+card.id = 'card'
+card.classList = 'card'
+card.appendChild (card) */
+
+const criarCard = () => {
+     const card = document.createElement('div')
+     card.id = 'card'
+     card.classList = 'card'
+     container.appendChild(card)
+}
 
